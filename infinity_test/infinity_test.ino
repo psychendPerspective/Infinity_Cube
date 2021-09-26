421372a9b75348f87c513462d4b6d65de9a92e1d
psychendPerspective <vishal.bhat09@gmail.com>
2021-09-27 01:18:44 +0530

    adds rgb color code changes

--- infinity_test/infinity_test.ino
+++ infinity_test/infinity_test.ino
@@ -1,20 +1,65 @@
 #include <FastLED.h>
 
-#define LED_PIN     5
-#define NUM_LEDS    144
+#define LED_PIN     3
+#define NUM_LEDS    60
 
 CRGB leds[NUM_LEDS];
 int counter;
+//RGB values
+uint8_t red_color_code; //between 0-255
+uint8_t green_color_code; //between 0-255
+uint8_t blue_color_code; //between 0-255
+
 void setup() 
 {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   counter=0;
+  red_color_code = 0;
+  green_color_code = 120;
+  blue_color_code = 120;
+  Serial.begin(115200);
   
 }
-void loop() {  
-  counter=(counter+1)%144;
-  leds[counter] = CRGB(255, 0, 0);
-  FastLED.show();
-  delay(20);  
-  leds[counter] = CRGB(0, 0, 0);
+
+void loop() 
+{
+    for(counter=0; counter<=NUM_LEDS; counter++)
+    {
+      //counter=(counter+1)%NUM_LEDS;
+      leds[counter] = CRGB(red_color_code, green_color_code, blue_color_code);
+      FastLED.show();
+      if(counter == NUM_LEDS)
+      {
+        counter = 0;
+        red_color_code = red_color_code + 5;
+        blue_color_code = blue_color_code - 5;
+        green_color_code = green_color_code - 5;
+//        red_color_code++;
+//        blue_color_code--;
+//        green_color_code--;
+      }
+      delay(100);  
+      leds[counter] = CRGB(0, 0, 0);
+      if(red_color_code == 255)
+      {
+        red_color_code = 0;
+      }
+      if(green_color_code == 0)
+      {
+        green_color_code = 255;
+      }
+      if(blue_color_code == 0)
+      {
+        blue_color_code = 255;
+      }
+      
+      Serial.print("red_color_code : ");
+      Serial.println(red_color_code);
+      Serial.print("green_color_code : ");
+      Serial.println(green_color_code);
+      Serial.print("blue_color_code : ");
+      Serial.println(blue_color_code);
+      Serial.print("Counter is:");
+      Serial.println(counter);
+    }
 }
